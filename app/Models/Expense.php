d03f5fd90dd52b6a70e43bdccd5daa4356414de8
siam02 <siam.jaman02@gmail.com>
2025-08-24 23:09:00 +0600

    Expense model added with relation Category

---
+++ app/Models/Expense.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'category_id', 'title', 'amount', 'date'];
+
+    protected $casts = [
+        'date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
